98fd9c4698206bc7387d6f31d3b55f50bead4d63
Michele <michele.belletti@elettra.eu>
2026-07-13 14:50:07 +0200

    add rust working code

--- src/braccio_planning/src/send_traj.rs
+++ src/braccio_planning/src/send_traj.rs
@@ -1,10 +1,31 @@
 
 
 
-use rclrs::*
+// use rclrs::*
 // std_msgs::msg::String as StringMsg;
+// use ros_env::std_msgs::msg::String;
 
+use anyhow::{Error, Result};
+use rclrs::*;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Error> {
+    let context = Context::default_from_env()?;
+    let executor = context.create_basic_executor();
+
+    let node = executor.create_node("minimal_publisher")?;
+
+    let publisher = node.create_publisher::< ros_env::std_msgs::msg::String>("pub_msg")?;
+
+    let mut message =  ros_env::std_msgs::msg::String::default();
+
+    let mut publish_count: u32 = 1;
+
+    while context.ok() {
+        message.data = format!("Hello, world! {}", publish_count);
+        println!("Publishing: [{}]", message.data);
+        publisher.publish(&message)?;
+        publish_count += 1;
+        std::thread::sleep(std::time::Duration::from_millis(500));
+    }
+    Ok(())
 }
